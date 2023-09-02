9bc657443a86ba6d5772e031baeec90e471f548f
ferrisdavid <davidferris195@gmail.com>
2023-09-02 00:05:59 -0400

    Added port scan function + thread spawning to handle scanning of different ports + final command line output for open ports

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::{env, process};
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
 
+// Constant Max Port to Scan. 
+const MAX_PORT: u16 = 65535;
+
+// Program Command Line Arguments Struct.
 struct Arguments {
-    flag: String,
     ipaddr: IpAddr,
     threads: u16
 }
@@ -19,7 +25,7 @@ impl Arguments {
 
         let flag_arg = args[1].clone();
         if let Ok(ipaddr) = IpAddr::from_str(&flag_arg) {
-            return Ok(Arguments { flag: String::from(""), ipaddr, threads: 4 });
+            return Ok(Arguments { ipaddr, threads: 4 });
         }
         else {
             let flag = args[1].clone();
@@ -39,12 +45,32 @@ impl Arguments {
                     Ok(s) => s,
                     Err(_) => return Err("Failed to parse thread number.")
                 };
-                return Ok(Arguments { flag, ipaddr, threads });
+                return Ok(Arguments { ipaddr, threads });
             }
             else {
                 return  Err("invalid usage.");
             }
         }
+    }
+}
+
+// Port Scan Function - Scans a Range of ports from a specified start port based on the number of active threads. Each thread will call this function to a handle a range of ports concurrently.
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        };
+
+        if (MAX_PORT - port) < num_threads {
+            break;
+        }
+        port += num_threads;
     }
 }
 
@@ -65,5 +91,27 @@ fn main() {
         }
     );
 
-    println!("Hello, world!");
+    let num_threads = arguments.threads;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, arguments.ipaddr, num_threads);
+        });
+    }
+
+    // Collect the ports sent to receiver into output vector.
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    // Print Open Ports
+    println!("");
+    out.sort();
+    for port in out {
+        println!("{} is open", port);
+    }
 }
